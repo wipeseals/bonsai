477af8a3f1b27ecc5b7ab14367dc684a8a1c14f0
wipeseals <wipeseals@gmail.com>
2024-11-22 18:24:17 +0900

    feat(wip): Reg定義考え中

--- ruskv/top.py
+++ ruskv/top.py
@@ -1,9 +1,49 @@
-from amaranth import Module, Signal
-from amaranth.lib import wiring
+from amaranth import Module, Signal, unsigned
+from amaranth.lib import wiring, enum, data
 from amaranth.lib.wiring import In, Out
 from amaranth.cli import main
 
 from counter import Counter
+
+
+class InstType(enum.Enum):
+    # R-type
+    R = 0
+    # I-type
+    I = 1  # noqa: E741
+    # S-type
+    S = 2
+    # B-type
+    B = 3
+    # U-type
+    U = 4
+    # J-type
+    J = 5
+
+
+class RegLayout(data.StructLayout):
+    """
+    x0 - x31 + pc までのレジスタ
+    """
+
+    def __init__(self, width=32, xlen=32):
+        self.width = width
+        self.xlen = xlen
+
+        super().__init__(
+            {
+                "x": data.ArrayLayout(unsigned(width), xlen),
+                "pc": unsigned(width),
+            }
+        )
+
+    def __call__(self, target):
+        return RegView(self, target)
+
+
+class RegView(data.View):
+    def x(self, index):
+        return self["x"][index]
 
 
 class Top(wiring.Component):
