972e761f77ce9fb4bdbcd34112f52480b66fd744
wipeseals <wipeseals@gmail.com>
2024-11-24 22:14:32 +0900

    feat(wip): 組み合わせ回路で試作

--- bonsai/instfetch.py
+++ bonsai/instfetch.py
@@ -1,75 +1,84 @@
-from amaranth import Module, Signal, unsigned
-from amaranth.lib import wiring, enum, data
+from typing import Any
+from amaranth import Module, Shape, Signal, unsigned
+from amaranth.lib import wiring, enum, data, memory
 from amaranth.lib.wiring import In, Out
 from amaranth.cli import main
 
 
-
-class InstType(enum.Enum):
-    # R-type
-    R = 0
-    # I-type
-    I = 1  # noqa: E741
-    # S-type
-    S = 2
-    # B-type
-    B = 3
-    # U-type
-    U = 4
-    # J-type
-    J = 5
-
-
-class RegLayout(data.StructLayout):
+class SimpleInstMem(wiring.Component):
     """
-    x0 - x31 + pc までのレジスタ
+    SimpleInstMem is a simple instruction memory component that reads instructions from a memory block.
+    Attributes:
+        rd_addr (In): Input signal that specifies the address to read from.
+        rd_data (Out): Output signal that contains the instruction read from memory.
     """
 
-    def __init__(self, width=32, xlen=32):
-        self.width = width
-        self.xlen = xlen
-
+    def __init__(self, data_shape: Shape, depth: int, init_data: Any = [0x00000000]):
+        self.mem = memory.Memory(shape=data_shape, depth=depth, init=init_data)
         super().__init__(
             {
-                "x": data.ArrayLayout(unsigned(width), xlen),
-                "pc": unsigned(width),
+                "rd_addr": In(depth.bit_length()),
+                "rd_data": Out(data_shape),
             }
         )
 
-    def __call__(self, target):
-        return RegView(self, target)
-
-
-class RegView(data.View):
-    def x(self, index):
-        return self["x"][index]
-
+    def elaborate(self, platform):
+        m = Module()
+        m.submodules.mem = self.mem
+        m.d.comb += self.rd_data.eq(self.mem[self.rd_addr])
+        return m
 
 
 class InstFetch(wiring.Component):
     """
-    Instruction Fetch Unit
+    InstFetch is a hardware component that fetches instructions from memory.
     Attributes:
-        pc (In): Program Counter
-        inst (Out): Instruction
+        pc (Signal): The current program counter value.
+        simple_inst_mem (SimpleInstMem): The instruction memory component.
+        next_pc (In): The next program counter value.
+        pc_out (Out): The current program counter value.
+        inst_out (Out): The instruction read from memory.
     """
 
-    def __init__(self, width: int = 32):
-        self.pc = Signal(width)
+    def __init__(
+        self,
+        data_shape: Shape,
+        # TODO: SimpleInstMemをBusMasterに変更する
+        inst_mem_depth: int = 256,
+        inst_mem_init_data: Any = [0x00000000],
+    ):
+        self.pc = Signal(data_shape)
 
-        self.next_pc = In(width)
-        self.pc_out = Out(width)
-        self.inst_out = Out(width)
+        # TODO: SimpleInstMemをBusMasterに変更する
+        self.simple_inst_mem = SimpleInstMem(
+            data_shape=data_shape, depth=inst_mem_depth, init_data=inst_mem_init_data
+        )
 
-        super().__init__()
+        super().__init__(
+            {
+                "next_pc": In(data_shape),
+                "pc_out": Out(data_shape),
+                "inst_out": Out(data_shape),
+                "valid": Out(1),
+            }
+        )
 
     def elaborate(self, platform):
         m = Module()
+
+        # 組み合わせ回路で現在のPCと対応するメモリアドレスの値をセットで出力
         m.d.comb += self.pc_out.eq(self.pc)
+        m.d.comb += self.inst_out.eq(self.simple_inst_mem.rd_data)
+        m.d.comb += self.simple_inst_mem.rd_addr.eq(self.pc)
+        # simple_inst_memをMemPortに置き換えた場合、リソース競合によるハザードを考慮する
+        m.d.comb += self.valid.eq(1)
+
+        with m.If(self.next_pc != self.pc):
+            m.d.sync += self.pc.eq(self.next_pc)
 
         return m
 
 
 if __name__ == "__main__":
-    instfetch = InstFetch(width=32)
+    instfetch = InstFetch(data_shape=unsigned(32), inst_mem_init_data=[0x00000000])
     main(instfetch)
