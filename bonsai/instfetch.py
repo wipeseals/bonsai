5bc8332e601671bdb4f6bb2916c2b9adbe0438c8
wipeseals <wipeseals@gmail.com>
2024-11-26 00:21:10 +0900

    refactor(wip): SynthConfigばらまく方式は一旦諦め

--- bonsai/instfetch.py
+++ bonsai/instfetch.py
@@ -4,177 +4,73 @@ from amaranth.lib import wiring, enum, data, memory, stream
 from amaranth.lib.wiring import In, Out
 from amaranth.cli import main
 
+import config
 
-class SimpleInstMem(wiring.Component):
+
+class InstFetchCtrl(enum.Enum):
     """
-    SimpleInstMem is a simple instruction memory component that reads instructions from a memory block.
-    Attributes:
-        rd_addr (In): Input signal that specifies the address to read from.
-        rd_data (Out): Output signal that contains the instruction read from memory.
+    Control signals for PC operation
     """
 
-    def __init__(self, data_shape: Shape, depth: int, init_data: Any = [0x00000000]):
-        self.data_shape = data_shape
-
-        # memory実体はdata_shape単位で確保されているが、アドレスはbyte単位
-        # 32bit=4byte, 64bit=8byteごとにメモリアクセスする, Compressとの動的切り替えは未対応
-        self.inst_bytes = self.data_shape.width // 8
-        self.mem = memory.Memory(shape=data_shape, depth=depth, init=init_data)
-        super().__init__(
-            {
-                "rd_addr": In(depth.bit_length()),
-                "rd_data": Out(data_shape),
-                "rd_valid": Out(1),
-            }
-        )
-
-    def elaborate(self, platform):
-        m = Module()
-        m.submodules.mem = self.mem
-
-        # メモリアドレスをbyte単位からdata_shape単位に変換して、メモリからデータを読み出す
-        rd_port = self.mem.read_port()
-        m.d.comb += rd_port.addr.eq(self.rd_addr // self.inst_bytes)
-        m.d.comb += self.rd_data.eq(rd_port.data)
-        # simple_inst_memをMemPortに置き換えた場合、リソース競合によるハザードを考慮する用の信号
-        m.d.comb += self.rd_valid.eq(1)
-
-        return m
+    # NOP: No operation
+    NOP = 0
+    # INC2: Increment by 2
+    INC2 = 1
+    # INC4: Increment by 4
+    INC4 = 2
+    # INC8: Increment by 8
+    INC8 = 3
+    # FLUSH: Flush the pipeline, reset the PC
+    FLUSH = 4
 
 
-class InstFetchDebug(data.StructLayout):
+class InstFetchDebug(data.Struct):
     """
     Debug information during instruction fetch
-    Attributes:
-        cyc (Signal): The cycle count at the time of fetch
-        seq_no (Signal): The sequence number of the fetch operation
     """
 
-    def __init__(self, data_shape: Shape):
-        ports = {
-            "cyc": data_shape,
-            "seq_no": data_shape,
-        }
-        super().__init__(ports)
+    cyc: config.REG_SHAPE
+    seq_no: config.REG_SHAPE
 
 
-class InstFetchIn(data.StructLayout):
+class InstFetchIn(data.Struct):
     """
     Data structure of IF input signals
-    Attributes:
-        pc (Signal): The next program counter value
     """
 
-    def __init__(self, data_shape: Shape, is_debug: bool = False):
-        ports = {
-            "pc": data_shape,
-        }
-        if is_debug:
-            # TODO: EX,WB からPC操作時のデバッグ情報があれば追加
-            pass
-
-        super().__init__(ports)
+    next_pc: config.ADDR_SHAPE
+    req: InstFetchCtrl
 
 
-class InstFetchOut(data.StructLayout):
+class InstFetchOut(data.Struct):
     """
     Data structure of the IF/ID pipeline register
-    Attributes:
-        inst (Signal): The instruction read from memory.
-        pc (Signal): The current program counter value.
-        debug (InstFetchDebug): Debug information during instruction fetch (optional)
     """
 
-    def __init__(self, data_shape: Shape, is_debug: bool = False):
-        ports = {
-            "inst": data_shape,
-            "pc": data_shape,
-        }
-        if is_debug:
-            ports["debug"] = data.Signal(InstFetchDebug(data_shape))
-
-        super().__init__(ports)
+    inst: config.INST_SHAPE
+    pc: config.ADDR_SHAPE
+    flush: unsigned(1)
+    debug: InstFetchDebug
 
 
 class InstFetch(wiring.Component):
     """
     InstFetch is a hardware component that fetches instructions from memory.
-    Attributes:
-        en (In): Input signal to enable instruction fetch.
-        pc (In): Input signal that specifies the next program counter value.
-        out (Out): Output signal that contains the instruction read from memory.
     """
 
-    def __init__(
-        self,
-        data_shape: Shape,
-        # TODO: SimpleInstMemをBusMasterに変更する
-        inst_mem_depth: int = 256,
-        inst_mem_init_data: Any = [0x00000000],
-        is_debug: bool = False,
-    ):
-        # meta data
-        self.data_shape = data_shape
-        self.is_debug = is_debug
-
-        # TODO: SimpleInstMemをBusMasterに変更する
-        self.mem = SimpleInstMem(
-            data_shape=data_shape, depth=inst_mem_depth, init_data=inst_mem_init_data
-        )
-
-        ports = {
-            "input": In(
-                stream.Signature(InstFetchIn(data_shape, is_debug=is_debug)).flip()
-            ),
-            "output": Out(
-                stream.Signature(InstFetchOut(data_shape, is_debug=is_debug))
-            ),
-        }
-        if is_debug:
-            self.debug = Signal(InstFetchDebug(data_shape))
-
-        super().__init__(ports)
+    pc: Signal(config.ADDR_SHAPE)
+    mem_read_port: memory.ReadPort.Signature(
+        addr_width=config.ADDR_WIDTH, shape=config.DATA_SHAPE
+    )
+    ctrl_in: In(stream.Signature(InstFetchIn))
+    ctrl_out: Out(stream.Signature(InstFetchOut))
+    debug: Signal(InstFetchDebug)
 
     def elaborate(self, platform):
         m = Module()
-        m.submodules.mem = self.mem
-
-        ###########################################################
-        # cycle数はenable問わずにカウントアップする
-        if self.is_debug:
-            m.d.sync += self.debug.cyc.eq(self.debug.cyc + 1)
-
-        ###########################################################
-        # PC: メモリアクセス完了 & 次段が受付可能なときに次のPCを読み込む
-
-        # 次段にデータ出していない
-        output_not_valid = ~self.output.valid
-
-        # 次段にデータ出しているが受け取ってない
-        output_accept = self.output.valid | self.output.ready
-        if self.is_debug:
-            # 結果送信完了時にシーケンス番号を更新
-            with m.If(output_accept):
-                m.d.sync += self.debug.seq_no.eq(self.debug.seq_no + 1)
-
-        # validになった後はreadyで受け取るまでは変わらない保証がある
-        m.d.comb += self.mem.rd_addr.eq(self.input.payload.pc)
-        # pcの更新は、Readが終わり次段が結果を受取るまで行わない
-        m.d.comb += self.input.ready.eq(
-            self.mem.rd_valid & (output_not_valid | output_accept)
-        )
-
-        ###########################################################
-        # inst/pc出力: メモリアクセス先と読み出したデータ
-        m.d.comb += self.output.payload.inst.eq(self.mem.rd_data)
-        m.d.comb += self.output.payload.pc.eq(self.mem.rd_addr)
-        if self.is_debug:
-            m.d.sync += self.output.payload.debug.eq(self.debug)
-        m.d.comb += self.output.valid.eq(self.mem.rd_valid)
-
         return m
 
 
 if __name__ == "__main__":
-    instfetch = InstFetch(data_shape=unsigned(32), inst_mem_init_data=[0x00000000])
+    instfetch = InstFetch()
     main(instfetch)
