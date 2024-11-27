e08d4917d7ff196b48d22635f2cbd293c4e90c9a
wipeseals <wipeseals@gmail.com>
2024-11-28 00:19:19 +0900

    feat: IS stage実装

--- bonsai/pipeline.py
+++ bonsai/pipeline.py
@@ -74,9 +74,16 @@ class IfIsReg(data.Struct):
         """
         return [
             self.ctrl.en.eq(1),
-            self.ctrl.debug.eq(debug),
             self.addr.eq(addr),
-            Print(Format("[IF] push  addr: {:016x}", addr)),
+            self.ctrl.debug.eq(debug),
+            Print(
+                Format(
+                    "[IF] push  cyc:{:016x} seqno:{:016x} addr:{:016x}",
+                    debug.cyc,
+                    debug.seqno,
+                    addr,
+                )
+            ),
         ]
 
     def stall(self):
@@ -84,9 +91,9 @@ class IfIsReg(data.Struct):
         Stall the instruction fetch
         """
         return [
-            self.ctrl.en.eq(0),
-            self.addr.eq(0),
-            Print(Format("[IF] stall addr: {:016x}", 0)),
+            self.ctrl.en.eq(0),  # 次段を止める
+            # self.addr.eq(0), # Don't care
+            Print("[IF] stall"),
         ]
 
     def flush(self):
@@ -94,9 +101,9 @@ class IfIsReg(data.Struct):
         Flush the instruction fetch
         """
         return [
-            self.ctrl.en.eq(0),
-            self.addr.eq(0),
-            Print(Format("[IF] flush addr: {:016x}", 0)),
+            self.ctrl.en.eq(0),  # 現状設計は0 dataのfetchはさせない
+            self.addr.eq(0),  # 明示的にクリア
+            Print("[IF] flush"),
         ]
 
 
@@ -108,11 +115,55 @@ class IsRfReg(data.Struct):
     # Control signals
     ctrl: StageCtrl
 
+    # Instruction Address
+    addr: config.ADDR_SHAPE
+
     # Instruction Data
     inst: config.INST_SHAPE
 
-    # Instruction Address
-    addr: config.ADDR_SHAPE
+    def push(
+        self, addr: config.ADDR_SHAPE, inst: config.INST_SHAPE, debug: StageCtrlDebug
+    ):
+        """
+        Push the instruction fetch address
+        """
+        return [
+            self.ctrl.en.eq(1),
+            self.addr.eq(addr),
+            self.inst.eq(inst),
+            self.ctrl.debug.eq(debug),
+            Print(
+                Format(
+                    "[IS] push  cyc:{:016x} seqno:{:016x} addr:{:016x} inst:{:016x}",
+                    debug.cyc,
+                    debug.seqno,
+                    addr,
+                    inst,
+                )
+            ),
+        ]
+
+    def stall(self):
+        """
+        Stall the instruction fetch
+        """
+        return [
+            self.ctrl.en.eq(0),  # 次段を止める
+            # self.addr.eq(0), # Don't care
+            # self.inst.eq(0), # Don't care
+            Print("[IS] stall"),
+        ]
+
+    def flush(self):
+        """
+        Flush the instruction fetch
+        """
+        return [
+            self.ctrl.en.eq(0),  # 現状設計は0 dataのfetchはさせない
+            self.addr.eq(0),  # 明示的にクリア
+            self.inst.eq(0),  # 明示的にクリア
+            Print("[IS] flush"),
+        ]
 
 
 class RfExReg(data.Struct):
--- bonsai/stage.py
+++ bonsai/stage.py
@@ -10,7 +10,7 @@ import util
 
 class IfStage(wiring.Component):
     """
-    Instruction Fetch Stage
+    Instruction Fetch First Stage
     """
 
     input: In(pipeline.IfReg)
@@ -19,6 +19,8 @@ class IfStage(wiring.Component):
 
     def elaborate(self, platform):
         m = Module()
+
+        # 型定義得るために追加
         input: pipeline.IfReg = self.input
         output: pipeline.IfIsReg = self.output
         side_ctrl: pipeline.SideCtrl = self.side_ctrl
@@ -33,9 +35,8 @@ class IfStage(wiring.Component):
             m.d.sync += output.flush()
         with m.Else():
             with m.If(input.ctrl.en):
-                # pc
-                m.d.sync += output.push(input.pc, debug)
-                # debug info
+                m.d.sync += output.push(addr=input.pc, debug=debug)
+                # fetch sequence number更新
                 m.d.sync += debug.seqno.eq(debug.seqno + 1)
             with m.Else():
                 m.d.sync += output.stall()
@@ -43,5 +44,61 @@ class IfStage(wiring.Component):
         return m
 
 
+class IsStage(wiring.Component):
+    """
+    Instruction Fetch Second Stage
+    """
+
+    input: In(pipeline.IfIsReg)
+    output: Out(pipeline.IsRfReg)
+    side_ctrl: In(pipeline.SideCtrl)
+
+    def __init__(self, init_data: Any = []):
+        self._init_data = init_data
+        super().__init__()
+
+    def elaborate(self, platform):
+        m = Module()
+
+        # 型定義得るために追加
+        input: pipeline.IfIsReg = self.input
+        output: pipeline.IsRfReg = self.output
+        side_ctrl: pipeline.SideCtrl = self.side_ctrl
+
+        # L1 Cache Body
+        m.submodules.mem = mem = memory.Memory(
+            shape=config.INST_SHAPE, depth=config.L1_CACHE_DEPTH, init=self._init_data
+        )
+
+        # TODO: 外部ポートを追加し、キャッシュできるようにする
+        # wr_port = mem.write_port()
+
+        # Pipeline Register間にFF挟む必要ないので、直接接続かつ非同期で構成
+        rd_port = mem.read_port(domain="comb")
+        m.d.comb += [
+            # memはINST_SHAPEの配列で確保してあるので、下位ビットを落とす
+            rd_port.addr.eq(input.addr.shift_right(config.INST_ADDR_SHIFT)),
+        ]
+
+        # Push Instruction fetch address
+        with m.If(side_ctrl.clr):
+            # stall中にflushがかかった場合は、flushを優先する
+            m.d.sync += output.flush()
+        with m.Else():
+            with m.If(input.ctrl.en):
+                # TODO: CacheMiss時の処理を追加
+
+                # メモリから出力されている命令を次のステージに渡す
+                m.d.sync += output.push(
+                    addr=input.addr, inst=rd_port.data, debug=input.ctrl.debug
+                )
+            with m.Else():
+                m.d.sync += output.stall()
+
+        return m
+
+
 if __name__ == "__main__":
-    util.export_verilog_file(IfStage(), f"{IfStage.__name__}")
+    stages = [IfStage(), IsStage()]
+    for stage in stages:
+        util.export_verilog_file(stage, f"{stage.__class__.__name__}")
