4345bd3e65a81a1ee5a4b4a9b6503d1e635d305c
wipeseals <wipeseals@gmail.com>
2024-11-28 00:24:25 +0900

    test: IS stageにstallテストを追加

--- tests/test_stage_is.py
+++ tests/test_stage_is.py
@@ -1,3 +1,4 @@
+from operator import is_
 from amaranth import Const
 from bonsai import config
 from bonsai.stage import IsStage
@@ -31,3 +32,35 @@ def test_is_freerun():
             assert ctx.get(dut.output.ctrl.debug.seqno) == i
 
     run_sim(f"{test_is_freerun.__name__}", dut=dut, testbench=bench)
+
+
+def test_is_stall():
+    inst_test_data = generate_test_data()
+    dut = IsStage(init_data=inst_test_data)
+
+    async def bench(ctx):
+        stall_start_cyc = 10
+        stall_end_cyc = 20
+        for i in range(config.L1_CACHE_DEPTH):
+            is_stall = stall_start_cyc <= i < stall_end_cyc
+            addr = i * 4
+            if is_stall:
+                ctx.set(dut.input.ctrl.en, 0)
+            else:
+                ctx.set(dut.input.ctrl.en, 1)
+            ctx.set(dut.input.addr, addr)
+            ctx.set(dut.input.ctrl.debug.cyc, i)
+            ctx.set(dut.input.ctrl.debug.seqno, i)
+            ctx.set(dut.side_ctrl.clr, 0)
+            await ctx.tick()
+            if is_stall:
+                # assert ctx.get(dut.output.addr) == 0 # Don't care
+                # assert ctx.get(dut.output.inst) == 0 # Don't care
+                assert ctx.get(dut.output.ctrl.en) == 0
+            else:
+                assert ctx.get(dut.output.addr) == addr
+                assert ctx.get(dut.output.inst) == inst_test_data[i].value
+                assert ctx.get(dut.output.ctrl.en) == 1
+                assert ctx.get(dut.output.ctrl.debug.seqno) == i
+
+    run_sim(f"{test_is_stall.__name__}", dut=dut, testbench=bench)
