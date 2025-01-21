dd34ca61fda5764d7add484be1a4e176a4a6c128
wipeseals <wipeseals@gmail.com>
2025-01-21 23:26:23 +0900

    feat: test_is_incrementにパラメータ化を追加し、num_inst_bytesを動的に設定

--- tests/test_stage_is.py
+++ tests/test_stage_is.py
@@ -1,13 +1,15 @@
 from bonsai.pipeline_stage import InstSelectStage
 
 from tests.testutil import run_sim
+import pytest
 
 INITIAL_PC: int = 0x1000
 INITIAL_UNIQ_ID: int = 0x1234
 LANE_ID: int = 0
 
 
-def test_is_increment():
+@pytest.mark.parametrize("num_inst_bytes", [2, 4, 8])
+def test_is_increment(num_inst_bytes: int):
     dut = InstSelectStage(
         initial_pc=INITIAL_PC,
         initial_uniq_id=INITIAL_UNIQ_ID,
@@ -22,8 +24,8 @@ def test_is_increment():
         # no branch
         ctx.set(dut.prev_req.branch_req.en, 0)
         ctx.set(dut.prev_req.branch_req.next_pc, 0)
-        # 4byte inst
-        ctx.set(dut.prev_req.num_inst_bytes, 4)
+        # set num_inst_bytes
+        ctx.set(dut.prev_req.num_inst_bytes, num_inst_bytes)
 
         for cyc in range(30):
             await ctx.tick()
@@ -32,8 +34,8 @@ def test_is_increment():
             assert ctx.get(dut.next_req.ctrl.stall) == 0
             assert ctx.get(dut.next_req.ctrl.flush) == 0
             # check pc
-            assert ctx.get(dut.next_req.locate.pc) == INITIAL_PC + 4 * cyc
+            assert ctx.get(dut.next_req.locate.pc) == INITIAL_PC + num_inst_bytes * cyc
             assert ctx.get(dut.next_req.locate.uniq_id) == INITIAL_UNIQ_ID + cyc
-            assert ctx.get(dut.next_req.locate.num_inst_bytes) == 4
+            assert ctx.get(dut.next_req.locate.num_inst_bytes) == num_inst_bytes
 
-    run_sim(f"{test_is_increment.__name__}", dut=dut, testbench=bench)
+    run_sim(f"{test_is_increment.__name__}_{num_inst_bytes}", dut=dut, testbench=bench)
