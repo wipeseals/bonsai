cea1af425e42cd09a69d290ad74fe232b51ff66e
wipeseals <wipeseals@gmail.com>
2025-01-21 23:24:49 +0900

    feat: InstFetchStageをInstSelectStageに変更し、テストを更新

--- tests/test_stage_is.py
+++ tests/test_stage_is.py
@@ -1,26 +1,39 @@
-from bonsai.pipeline_stage import InstFetchStage
+from bonsai.pipeline_stage import InstSelectStage
 
 from tests.testutil import run_sim
 
+INITIAL_PC: int = 0x1000
+INITIAL_UNIQ_ID: int = 0x1234
+LANE_ID: int = 0
+
 
 def test_is_increment():
-    dut = InstFetchStage()
+    dut = InstSelectStage(
+        initial_pc=INITIAL_PC,
+        initial_uniq_id=INITIAL_UNIQ_ID,
+        lane_id=LANE_ID,
+    )
 
     async def bench(ctx):
-        # initial
-        addr = 0xAA
-        ctx.set(dut.input.ctrl.en, 1)
-        ctx.set(dut.side.clr, 0)
-        ctx.set(dut.input.pc, addr)
-        await ctx.tick()
-        assert ctx.get(dut.output.addr) == addr
-        assert ctx.get(dut.output.ctrl.en) == 1
-        # jump
-        addr = 0x55
-        ctx.set(dut.input.ctrl.en, 1)
-        ctx.set(dut.input.pc, addr)
-        await ctx.tick()
-        assert ctx.get(dut.output.addr) == addr
-        assert ctx.get(dut.output.ctrl.en) == 1
+        # enable & no flush/stall
+        ctx.set(dut.prev_req.en, 1)
+        ctx.set(dut.prev_req.ctrl.stall, 0)
+        ctx.set(dut.prev_req.ctrl.flush, 0)
+        # no branch
+        ctx.set(dut.prev_req.branch_req.en, 0)
+        ctx.set(dut.prev_req.branch_req.next_pc, 0)
+        # 4byte inst
+        ctx.set(dut.prev_req.num_inst_bytes, 4)
 
-    run_sim(f"{test_if_jump.__name__}", dut=dut, testbench=bench)
+        for cyc in range(30):
+            await ctx.tick()
+            # check enable
+            assert ctx.get(dut.next_req.en) == 1
+            assert ctx.get(dut.next_req.ctrl.stall) == 0
+            assert ctx.get(dut.next_req.ctrl.flush) == 0
+            # check pc
+            assert ctx.get(dut.next_req.locate.pc) == INITIAL_PC + 4 * cyc
+            assert ctx.get(dut.next_req.locate.uniq_id) == INITIAL_UNIQ_ID + cyc
+            assert ctx.get(dut.next_req.locate.num_inst_bytes) == 4
+
+    run_sim(f"{test_is_increment.__name__}", dut=dut, testbench=bench)
