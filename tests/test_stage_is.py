7cab1847d256d6fcaa31143dbbc30623ce12d5dc
wipeseals <wipeseals@gmail.com>
2025-01-21 23:38:16 +0900

    feat: test_is_flushを追加し、フラッシュ動作を検証

--- tests/test_stage_is.py
+++ tests/test_stage_is.py
@@ -101,3 +101,61 @@ def test_is_stall(stall_cyc: int, num_inst_bytes: int = 4):
             assert ctx.get(dut.next_req.locate.num_inst_bytes) == num_inst_bytes
 
     run_sim(f"{test_is_stall.__name__}", dut=dut, testbench=bench)
+
+
+def test_is_flush(num_inst_bytes: int = 4):
+    dut = InstSelectStage(
+        initial_pc=INITIAL_PC,
+        initial_uniq_id=INITIAL_UNIQ_ID,
+        lane_id=LANE_ID,
+    )
+
+    async def bench(ctx):
+        # enable & no flush/stall
+        ctx.set(dut.prev_req.en, 1)
+        ctx.set(dut.prev_req.ctrl.stall, 0)
+        ctx.set(dut.prev_req.ctrl.flush, 0)
+        # no branch
+        ctx.set(dut.prev_req.branch_req.en, 0)
+        ctx.set(dut.prev_req.branch_req.next_pc, 0)
+        # set num_inst_bytes
+        ctx.set(dut.prev_req.num_inst_bytes, num_inst_bytes)
+
+        pre_cyc = 3
+        for cyc in range(pre_cyc):
+            await ctx.tick()
+            # check enable
+            assert ctx.get(dut.next_req.en) == 1
+            assert ctx.get(dut.next_req.ctrl.stall) == 0
+            assert ctx.get(dut.next_req.ctrl.flush) == 0
+            # check pc
+            assert ctx.get(dut.next_req.locate.pc) == INITIAL_PC + num_inst_bytes * cyc
+            assert ctx.get(dut.next_req.locate.uniq_id) == INITIAL_UNIQ_ID + cyc
+            assert ctx.get(dut.next_req.locate.num_inst_bytes) == num_inst_bytes
+
+        # flush
+        ctx.set(dut.prev_req.ctrl.flush, 1)
+        await ctx.tick()
+        # check enable
+        assert ctx.get(dut.next_req.en) == 0
+        assert ctx.get(dut.next_req.ctrl.stall) == 0
+        assert ctx.get(dut.next_req.ctrl.flush) == 1
+
+        ctx.set(dut.prev_req.ctrl.flush, 0)
+        post_cyc = 3
+        for cyc in range(post_cyc):
+            await ctx.tick()
+            # check enable
+            assert ctx.get(dut.next_req.en) == 1
+            assert ctx.get(dut.next_req.ctrl.stall) == 0
+            assert ctx.get(dut.next_req.ctrl.flush) == 0
+            # check pc (keep pc/uniq_id)
+            assert ctx.get(dut.next_req.locate.pc) == INITIAL_PC + num_inst_bytes * (
+                cyc + pre_cyc
+            )
+            assert (
+                ctx.get(dut.next_req.locate.uniq_id) == INITIAL_UNIQ_ID + cyc + pre_cyc
+            )
+            assert ctx.get(dut.next_req.locate.num_inst_bytes) == num_inst_bytes
+
+    run_sim(f"{test_is_flush.__name__}", dut=dut, testbench=bench)
