8a54bdf64acde744c614584c086f6a3d63a63421
wipeseals <wipeseals@gmail.com>
2025-01-26 22:57:58 +0900

    test: SingleCycleMemoryに書き込みシーケンステストを追加

--- tests/test_lsu.py
+++ tests/test_lsu.py
@@ -10,7 +10,7 @@ def expect_data(seed: int = 0, depth: int = 128) -> List[int]:
     """
     Generate expected data
     """
-    return list([(~(seed + i)) & 0xFFFFFFFF for i in range(depth)])
+    return list([(seed + i) & 0xFFFFFFFF for i in range(depth)])
 
 
 def test_ssm_read_seq():
@@ -69,6 +69,48 @@ def test_ssm_read_random():
             assert ctx.get(dut.req_in.data_out) == exp_data
 
     run_sim(f"{test_ssm_read_seq.__name__}", dut=dut, testbench=bench)
+
+
+def test_ssm_write_seq():
+    init_data = expect_data(seed=0x0)
+    dut = SingleCycleMemory(init_data=init_data)
+
+    async def bench(ctx):
+        # disable
+        ctx.set(dut.req_in.op_type, MemoryOperationType.NOP.value)
+        ctx.set(dut.req_in.addr_in, 0)
+        ctx.set(dut.req_in.data_in, 0)
+
+        nop_cyc = 3
+        for _ in range(nop_cyc):
+            await ctx.tick()
+            assert ctx.get(dut.req_in.busy) == 0
+
+        # write incremental
+        def process_data(data: int) -> int:
+            return 0xFFFFFFFF - data
+
+        ctx.set(dut.req_in.op_type, MemoryOperationType.WRITE_CACHE.value)
+        for data_idx, exp_data in enumerate(init_data):
+            write_addr = data_idx * 4
+            write_data = process_data(exp_data)
+            ctx.set(dut.req_in.addr_in, write_addr)
+            ctx.set(dut.req_in.data_in, write_data)
+            await ctx.tick()
+            assert ctx.get(dut.req_in.busy) == 0
+            # 設計上はRead portはcombなのでWrite data貫通して出てくる
+            assert ctx.get(dut.req_in.data_out) == write_data
+
+        # read verify
+        ctx.set(dut.req_in.op_type, MemoryOperationType.READ_CACHE.value)
+        for data_idx, exp_data in enumerate(init_data):
+            read_addr = data_idx * 4
+            ctx.set(dut.req_in.addr_in, read_addr)
+            await ctx.tick()
+            assert ctx.get(dut.req_in.busy) == 0
+            # assert ctx.get(dut.req_in.data_out) == process_data(exp_data)
+
+    run_sim(f"{test_ssm_write_seq.__name__}", dut=dut, testbench=bench)
 
 
 @pytest.mark.parametrize("use_strict_assert", [True, False])
