d37765321d6bf9f48319438cad5ed9a7d09138b4
wipeseals <wipeseals@gmail.com>
2025-01-31 01:31:27 +0900

    refactor: WishboneSpecを使った初期化に書き換え

--- bonsai/core.py
+++ bonsai/core.py
@@ -33,6 +33,7 @@ class WishboneSpec:
         spec_rev (str): The revision of the Wishbone specification.
         support_err_i (bool): Whether error support is enabled.
         support_rty_i (bool): Whether retry support is enabled.
+        support_lock_o (bool): Whether lock support is enabled.
         support_tga_o (Optional[WishboneTag]): The tag support for the address bus.
         support_tgd_i (Optional[WishboneTag]): The tag support for the data input bus.
         support_tgd_o (Optional[WishboneTag]): The tag support for the data output bus.
@@ -41,20 +42,30 @@ class WishboneSpec:
     """
 
     port_size: Literal[8, 16, 32, 64]
-    granularity: Optional[Literal[8, 16, 32, 64]] = None
+    granularity: Literal[8, 16, 32, 64]
     spec_rev: str = "B4"
     support_err_i: bool = False
     support_rty_i: bool = False
+    support_lock_o: bool = False
     support_tga_o: Optional[WishboneTag] = None
     support_tgd_i: Optional[WishboneTag] = None
     support_tgd_o: Optional[WishboneTag] = None
     support_tgc_o: Optional[WishboneTag] = None
     endian: Optional[Literal["little", "big"]] = None
 
+    @property
+    def addr_width(self) -> int:
+        return self.port_size
+
+    @property
+    def data_width(self) -> int:
+        return self.granularity
+
+    @property
+    def sel_width(self) -> int:
+        return self.granularity // 8
+
     def __post_init_post_parse__(self):
-        # granularity が指定されていない場合は port_size と同じ値にする
-        if self.granularity is None:
-            self.granularity = self.port_size
         # enditan が指定されていない場合、port_size == granularity の場合のみ許容
         if self.endian is None:
             if self.port_size != self.granularity:
@@ -68,78 +79,65 @@ class WishboneSpec:
 
 class WishbonePort(wiring.Signature):
     """
-    WishbonePort is a class that represents a Wishbone bus port with specified address and data widths.
+    WishbonePort is a class that represents a Wishbone bus port with a specified specification.
     Attributes:
-        addr_width (int): The width of the address bus.
-        data_width (int): The width of the data bus.
-        sel_width (int): The width of the byte select bus.
-        tga_width (Optional[int]): The width of the address tag bus.
-        tgd_i_width (Optional[int]): The width of the data tag input bus.
-        tgd_o_width (Optional[int]): The width of the data tag output bus.
-        tgc_width (Optional[int]): The width of the cycle tag bus.
+        spec (WishboneSpec): The specification of the Wishbone bus.
     Methods:
-        __init__(addr_width: int, data_width: int, tga_width: Optional[int] = None, tgd_i_width: Optional[int] = None, tgd_o_width: Optional[int] = None, tgc_width: Optional[int] = None): Initializes the WishbonePort with the given address and data widths and optional tag widths.
+        __init__(spec: WishboneSpec): Initializes the WishbonePort with the given specification.
     """
 
     def __init__(
         self,
-        addr_width: int,
-        data_width: int,
-        tga_width: Optional[int] = None,
-        tgd_i_width: Optional[int] = None,
-        tgd_o_width: Optional[int] = None,
-        tgc_width: Optional[int] = None,
+        spec: WishboneSpec,
     ):
-        self.addr_width = addr_width
-        self.data_width = data_width
-        self.sel_width = addr_width // 8  # byte sel
-        self.tga_width = tga_width
-        self.tgd_i_width = tgd_i_width
-        self.tgd_o_width = tgd_o_width
-        self.tgc_width = tgc_width
+        self.spec = spec
 
         # Required
         members = {
             # clk_i は省略
             # data input array
-            "dat_i": In(self.data_width),
+            "dat_i": In(self.spec.data_width),
             # data output array
-            "dat_o": Out(self.data_width),
+            "dat_o": Out(self.spec.data_width),
             # wishbone interface reset
             "rst_i": In(1),
             # address output array
-            "adr_o": Out(self.addr_width),
+            "adr_o": Out(self.spec.addr_width),
             # write enable/read enable output
             "we_o": Out(1),
             # byte enable output array
-            "sel_o": Out(self.sel_width),
+            "sel_o": Out(self.spec.sel_width),
             # valid data transfer output
             "stb_o": Out(1),
             # bus cycle acknowledge input
             "ack_i": In(1),
             # bus cycle in progress output
             "cyc_o": Out(1),
-            # bus cycle uninterruptible input
-            "lock_o": Out(1),
-            # error input
-            "err_i": In(1),
-            # not ready input
-            "rty_i": Out(1),
         }
 
-        # Optional
-        if self.tga_width is not None:
+        # feature support (optional)
+        if self.spec.support_err_i:
+            # error input
+            members["err_i"] = In(1)
+        if self.spec.support_rty_i:
+            # not ready input
+            members["rty_i"] = Out(1)
+        if self.spec.support_lock_o:
+            # bus cycle uninterruptible input
+            members["lock_o"] = Out(1)
+        # tag support (optional)
+        if self.spec.support_tga_o is not None:
             # address tag output
-            members["tga_o"] = Out(self.tga_width)
-        if self.tgd_i_width is not None:
+            members["tga_o"] = Out(self.spec.support_tga_o.width)
+        if self.spec.support_tgd_i is not None:
             # data tag input
-            members["tgd_i"] = In(self.tgd_i_width)
-        if self.tgd_o_width is not None:
+            members["tgd_i"] = In(self.spec.support_tgd_i.width)
+        if self.spec.support_tgd_o is not None:
             # data tag output
-            members["tgd_o"] = Out(self.tgd_o_width)
-        if self.tgc_width is not None:
+            members["tgd_o"] = Out(self.spec.support_tgd_o.width)
+        if self.spec.support_tgc_o is not None:
             # cycle tag output
-            members["tgc_o"] = Out(self.tgc_width)
+            members["tgc_o"] = Out(self.spec.support_tgc_o.width)
 
         super().__init__(members)
 
