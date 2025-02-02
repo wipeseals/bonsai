7cf69e762a7fbb4fa84215fc78d6028a6e439375
wipeseals <wipeseals@gmail.com>
2025-02-02 14:57:19 +0900

    refactor: CLIをmain.pyに統合し、プラットフォームビルド機能を追加

---
+++ bonsai/main.py
@@ -0,0 +1,85 @@
+import argparse
+import logging
+from typing import List
+
+import util
+from amaranth import Elaboratable
+from amaranth.build.plat import Platform
+from amaranth.lib import wiring
+from amaranth_boards.arty_a7 import ArtyA7_35Platform
+from top import PlatformTop, Top
+
+
+def build(args: argparse.Namespace) -> None:
+    platform: Platform = None
+    if args.platform == "arty":
+        platform = ArtyA7_35Platform()
+    else:
+        logging.warning(f"Unsupported platform: {args.platform}")
+
+    if platform is not None:
+        logging.info(
+            f"Building for {platform.__class__.__name__}, do_build={not args.skip_build}, do_program={not args.skip_program}"
+        )
+        platform.build(
+            PlatformTop(),
+            do_build=not args.skip_build,
+            do_program=not args.skip_program,
+        )
+    else:
+        if args.skip_build:
+            logging.warning("No platform selected, skipping build")
+            return
+
+        logging.info("Generating Verilog files for all components")
+        target_components: List[Elaboratable] = [
+            Top(clk_freq=int(100e6), period_sec=1.0),
+        ]
+        for component in target_components:
+            filename = f"{component.__class__.__name__}"
+            logging.info(f"Generating {filename}.v")
+            util.export_verilog_file(
+                component=component,
+                name=filename,
+            )
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--log-level",
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Set the logging level",
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="action", help="Select the action to perform", required=True
+    )
+    parser_build = subparsers.add_parser("build", help="build the project")
+    parser_build.add_argument(
+        "--platform",
+        default="",
+        choices=["", "arty"],
+        help="Set the target platform",
+    )
+    parser_build.add_argument(
+        "--skip-build",
+        action="store_true",
+        help="Build the project",
+    )
+    parser_build.add_argument(
+        "--skip-program",
+        action="store_true",
+        help="Program the project",
+    )
+    parser_build.set_defaults(func=build)
+
+    # parse & run
+    args = parser.parse_args()
+    logging.basicConfig(level=args.log_level)
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
--- bonsai/top.py
+++ bonsai/top.py
@@ -1,16 +1,52 @@
-from amaranth import Module
+import logging
+
+from amaranth import Elaboratable, Module, Signal
+from amaranth.build.plat import Platform
 from amaranth.lib import wiring
-from bus import WishboneMaster
+from amaranth.lib.wiring import Out
+from amaranth.utils import ceil_log2
+from amaranth_boards.arty_a7 import ArtyA7_35Platform
 
 
 class Top(wiring.Component):
-    """
-    Top Component
-    """
+    def __init__(self, clk_freq: int, period_sec: float = 1.0):
+        self._freq = clk_freq
+        self._coutner_width = ceil_log2(self._freq)
+        self._blink_count = int(period_sec / (1 / float(clk_freq))) // 2
+        super().__init__(
+            {
+                "counter": Out(self._coutner_width),
+            }
+        )
 
-    def __init__(self):
-        super().__init__({})
-
-    def elaborate(self, platform):
+    def elaborate(self, platform: Platform) -> Module:
         m = Module()
+        # test counter
+        m.d.sync += self.counter.eq(self.counter + 1)
+        with m.If(self.counter == self._blink_count - 1):
+            m.d.sync += self.counter.eq(0)
+        return m
+
+
+class PlatformTop(Elaboratable):
+    def _elaborate_arty_a7_35(self, m: Module, platform: Platform, top: Top):
+        num_led = 4
+        leds = [platform.request("led", 0, dir="-") for i in range(num_led)]
+        buttons = [platform.request("button", i, dir="-") for i in range(num_led)]
+        switches = [platform.request("switch", i, dir="-") for i in range(num_led)]
+
+        m.submodules += leds + buttons + switches
+        for i in range(num_led):
+            m.d.comb += [leds[i].o.eq(top.counter[-i] ^ buttons[i].i ^ switches[i].i)]
+
+    def elaborate(self, platform: Platform) -> Module:
+        m = Module()
+        m.submodules.top = top = Top(platform.default_clk_frequency, period_sec=1.0)
+
+        # Platform specific elaboration
+        if isinstance(platform, ArtyA7_35Platform):
+            self._elaborate_arty_a7_35(m, platform, top)
+        else:
+            logging.warning(f"Unsupported platform: {platform}")
+
         return m
