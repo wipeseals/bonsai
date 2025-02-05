366adf48d6cd11c52af52518bbd62ed10ef06e7a
wipeseals <wipeseals@gmail.com>
2025-02-06 01:00:14 +0900

    feat(wip): VgaOutのロジック実装

--- bonsai/top.py
+++ bonsai/top.py
@@ -1,8 +1,9 @@
 import logging
+from dataclasses import dataclass
 
-from amaranth import Cat, Elaboratable, Module, Signal
+from amaranth import Cat, Elaboratable, Module, Mux, Signal, unsigned
 from amaranth.build.plat import Platform
-from amaranth.lib import enum, io, stream, wiring
+from amaranth.lib import data, enum, io, stream, wiring
 from amaranth.lib.wiring import In, Out
 from amaranth.utils import ceil_log2
 from amaranth_boards.arty_a7 import ArtyA7_35Platform
@@ -11,9 +12,211 @@ from periph.timer import Timer, TimerMode
 from periph.uart import UartTx
 
 
+class VideoPixelLayout(data.StructLayout):
+    def __init__(self, r_width: int, g_width: int, b_width: int):
+        super().__init__(
+            {
+                "r": unsigned(r_width),
+                "g": unsigned(g_width),
+                "b": unsigned(b_width),
+            }
+        )
+
+
+@dataclass
+class VgaConfig:
+    """
+    VGA timing parameters
+    """
+
+    # pixel width
+    width: int
+    # pixel height
+    height: int
+
+    # pixel data format
+    pixel_layout: VideoPixelLayout
+
+    # front porch (horizontal)
+    h_front_porch: int
+    # pulse width (horizontal sync)
+    h_pulse: int
+    # back porch (horizontal)
+    h_back_porch: int
+
+    # front porch (vertical)
+    v_front_porch: int
+    # pulse width (vertical sync)
+    v_pulse: int
+    # back porch (vertical)
+    v_back_porch: int
+
+    @property
+    def hsync_start(self) -> int:
+        """
+        HSYNC start offset
+        """
+        return self.h_front_porch
+
+    @property
+    def hsync_end(self) -> int:
+        """
+        HSYNC end offset
+        """
+        return self.h_front_porch + self.h_pulse
+
+    @property
+    def vsync_start(self) -> int:
+        """
+        VSYNC start offset
+        """
+        return self.v_front_porch
+
+    @property
+    def vsync_end(self) -> int:
+        """
+        VSYNC end offset
+        """
+        return self.v_front_porch + self.v_pulse
+
+    @property
+    def hdata_start(self) -> int:
+        """
+        データの開始位置 (水平方向)
+        """
+        return self.h_front_porch + self.h_pulse + self.h_back_porch
+
+    @property
+    def vdata_start(self) -> int:
+        """
+        データの開始位置 (垂直方向)
+        """
+        return self.v_front_porch + self.v_pulse + self.v_back_porch
+
+    @property
+    def hdata_end(self) -> int:
+        """
+        幅方向の合計ピクセル数 (フロントポーチ、バックポーチ、同期パルスを含む)
+        """
+        return self.width + self.h_front_porch + self.h_back_porch + self.h_pulse
+
+    @property
+    def vdata_end(self) -> int:
+        """
+        高さ方向の合計ピクセル数 (フロントポーチ、バックポーチ、同期パルスを含む)
+        """
+        return self.height + self.v_front_porch + self.v_back_porch + self.v_pulse
+
+    @property
+    def h_counter_width(self) -> int:
+        """
+        水平カウンタのビット幅
+        """
+        return int(ceil_log2(self.hdata_end))
+
+    @property
+    def v_counter_width(self) -> int:
+        """
+        垂直カウンタのビット幅
+        """
+        return int(ceil_log2(self.vdata_end))
+
+
+class VgaOut(wiring.Component):
+    def __init__(self, config: VgaConfig, *, src_loc_at=0):
+        self.config = config
+        super().__init__(
+            {
+                "en": In(1),
+                "de": Out(1),
+                "hsync": Out(1),
+                "vsync": Out(1),
+                "pixel": Out(config.pixel_layout),
+                "pos_x": Out(config.h_counter_width),
+                "pos_y": Out(config.v_counter_width),
+                "backlight": Out(1),
+            },
+            src_loc_at=src_loc_at,
+        )
+
+    def elaborate(self, platform: Platform) -> Module:
+        m = Module()
+
+        # 現在位置 (VGA信号中のcyc)
+        h_counter = Signal(self.config.h_counter_width)
+        # 現在位置Y (VGA信号中のline)
+        v_counter = Signal(self.config.v_counter_width)
+        # 現在位置が有効な描画範囲なら1
+        data_valid = Signal(1, reset=0)
+        # 有効なデータ範囲であれば、そのX座標
+        pos_x = Signal(self.config.h_counter_width, reset=0)
+        # 有効なデータ範囲であれば、そのY座標
+        pos_y = Signal(self.config.v_counter_width, reset=0)
+
+        # 現在のカウントから有効な信号を生成
+        m.d.comb += [
+            # 水平垂直同期
+            # [front-porch, pulse, back-porch] の範囲で有効
+            self.hsync.eq(
+                (self.config.hsync_start <= h_counter)
+                & (h_counter < self.config.hsync_end)
+            ),
+            self.vsync.eq(
+                (self.config.vsync_start <= v_counter)
+                & (v_counter < self.config.vsync_end)
+            ),
+            # データ有効範囲
+            # [back-porch, data, (next)front-porch] の範囲で有効
+            data_valid.eq(
+                (self.config.hdata_start <= h_counter)
+                & (h_counter < self.config.hdata_end)
+                & (self.config.vdata_start <= v_counter)
+                & (v_counter < self.config.vdata_end)
+            ),
+            self.de.eq(data_valid),
+            # データ位置
+            pos_x.eq(Mux(data_valid, h_counter - self.config.hdata_start, 0)),
+            pos_y.eq(Mux(data_valid, v_counter - self.config.vdata_start, 0)),
+            self.pos_x.eq(pos_x),
+            self.pos_y.eq(pos_y),
+            # enならBacklight on (調光ができるならあってもいいかも)
+            self.backlight.eq(self.en),
+        ]
+
+        # test data pattern
+        m.d.comb += [
+            self.pixel.r.eq(pos_x),
+            self.pixel.g.eq(pos_y),
+            self.pixel.b.eq(pos_x + pos_y),
+        ]
+
+        # for vertical { for horizontal }
+        with m.If(self.en):
+            # Horizontal counter
+            with m.If(h_counter < self.config.hdata_end - 1):
+                m.d.sync += h_counter.eq(h_counter + 1)
+            with m.Else():
+                m.d.sync += h_counter.eq(0)
+                # Vertical counter
+                with m.If(v_counter < self.config.vdata_end - 1):
+                    m.d.sync += v_counter.eq(v_counter + 1)
+                with m.Else():
+                    m.d.sync += v_counter.eq(0)
+        with m.Else():
+            # Reset counter & sync invalid
+            m.d.sync += [h_counter.eq(0), v_counter.eq(0)]
+
+        return m
+
+
 class Top(wiring.Component):
     def __init__(
-        self, clk_freq: float, period_sec: float = 1.0, baud_rate: int = 115200
+        self,
+        clk_freq: float,
+        period_sec: float = 1.0,
+        baud_rate: int = 115200,
+        *,
+        src_loc_at=0,
     ):
         self.timer = Timer(clk_freq=clk_freq, default_period_seconds=period_sec)
         self.uart_tx = UartTx(clk_freq=clk_freq, baud_rate=baud_rate)
@@ -22,7 +225,8 @@ class Top(wiring.Component):
             {
                 "ovf": Out(1),
                 "tx": Out(1),
-            }
+            },
+            src_loc_at=src_loc_at,
         )
 
     def elaborate(self, platform: Platform) -> Module:
@@ -108,6 +312,56 @@ class PlatformTop(Elaboratable):
         ]
 
     def _elabolate_tangnano_9k(self, m: Module, platform: Platform):
+        ##################################################################
+        # VGA
+        # https://wiki.sipeed.com/hardware/en/tang/Tang-Nano-9K/examples/rgb_screen.html
+        # https://mm.digikey.com/Volume0/opasdata/d220001/medias/docus/204/104990583_Web.pdf
+        vga_config = VgaConfig(
+            width=800,
+            height=480,
+            pixel_layout=VideoPixelLayout(5, 6, 5),
+            h_front_porch=210,
+            h_pulse=1,
+            h_back_porch=182,
+            v_front_porch=62,
+            v_pulse=5,
+            v_back_porch=6,
+        )
+        m.submodules.vga = vga = VgaOut(config=vga_config)
+        # m.submodules.clk27 = clk27 = io.Buffer(
+        #     "i", platform.request("clk27", 0, dir="-")
+        # ) # TODO: default_clkが後からresouce確保して2重になっている?
+        lcd = platform.request("lcd", 0, dir="-")
+        m.submodules.lcd_clk = lcd_clk = io.Buffer("o", lcd.clk)
+        m.submodules.lcd_hs = lcd_hs = io.Buffer("o", lcd.hs)
+        m.submodules.lcd_vs = lcd_vs = io.Buffer("o", lcd.vs)
+        m.submodules.lcd_de = lcd_de = io.Buffer("o", lcd.de)
+        m.submodules.lcd_backlight = lcd_backlight = io.Buffer(
+            "o", platform.request("lcd_backlight", 0, dir="-")
+        )
+
+        lcd_r = [io.Buffer("o", r) for r in lcd.r]
+        lcd_g = [io.Buffer("o", g) for g in lcd.g]
+        lcd_b = [io.Buffer("o", b) for b in lcd.b]
+        m.submodules += lcd_r
+        m.submodules += lcd_g
+        m.submodules += lcd_b
+        lcd_r_signals = Cat([r.o for r in lcd_r])
+        lcd_g_signals = Cat([g.o for g in lcd_g])
+        lcd_b_signals = Cat([b.o for b in lcd_b])
+        m.d.comb += [
+            # lcd_clk.o.eq(clk27.i), # TODO: PLL 27MHz -> 33.3333MHz
+            lcd_hs.o.eq(vga.hsync),
+            lcd_vs.o.eq(vga.vsync),
+            lcd_de.o.eq(vga.de),
+            lcd_r_signals.eq(vga.pixel.r),
+            lcd_g_signals.eq(vga.pixel.g),
+            lcd_b_signals.eq(vga.pixel.b),
+            lcd_backlight.o.eq(vga.backlight),
+        ]
+
+        ##################################################################
+        # LED/Button
         NUM_LED = 6
         leds = [
             io.Buffer("o", platform.request("led", i, dir="-")) for i in range(NUM_LED)
