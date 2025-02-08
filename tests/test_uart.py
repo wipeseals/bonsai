6fc8af5bdbe58c989e5194f0cf8b448468808f57
wipeseals <wipeseals@gmail.com>
2025-02-08 23:00:37 +0900

    test: uart_rxの受信テスト

--- tests/test_uart.py
+++ tests/test_uart.py
@@ -4,7 +4,7 @@ import math
 import pytest
 from amaranth.sim import SimulatorContext
 
-from bonsai.periph.uart import UartConfig, UartParity, UartTx
+from bonsai.periph.uart import UartConfig, UartParity, UartRx, UartTx
 from bonsai.util import Simulation, even_parity, odd_parity
 
 
@@ -16,14 +16,14 @@ from bonsai.util import Simulation, even_parity, odd_parity
         (100e6, 921600, 8, 1, UartParity.NONE),
     ],
 )
-def test_uart_tx_send_short(
+def test_uart_rx_recv_short(
     clk_freq: float,
     baudrate: int,
     num_data_bit: int,
     num_stop_bit: int,
     parity: UartParity,
 ):
-    run_uart_tx_send(
+    run_uart_rx_recv(
         clk_freq=clk_freq,
         baudrate=baudrate,
         num_data_bit=num_data_bit,
@@ -32,13 +32,103 @@ def test_uart_tx_send_short(
     )
 
 
-@pytest.mark.skip(reason="very slow test")
-@pytest.mark.parametrize("clk_freq", [10e6, 100e6])
-@pytest.mark.parametrize("baudrate", [9600, 115200, 230400])
-@pytest.mark.parametrize("num_data_bit", [8, 7])
-@pytest.mark.parametrize("num_stop_bit", [1, 2])
-@pytest.mark.parametrize("parity", [UartParity.NONE, UartParity.ODD, UartParity.EVEN])
-def test_uart_tx_send_long(
+def run_uart_rx_recv(
+    clk_freq: float,
+    baudrate: int,
+    num_data_bit: int,
+    num_stop_bit: int,
+    parity: UartParity,
+):
+    config = UartConfig(
+        clk_freq=clk_freq,
+        baud_rate=baudrate,
+        num_data_bit=num_data_bit,
+        num_stop_bit=num_stop_bit,
+        parity=parity,
+    )
+    dut = UartRx(config=config)
+    send_datas = list(
+        [x & ((1 << num_data_bit) - 1) for x in [0xFF, 0x00, 0xA5, 0x3C, 0xEF, 0x12]]
+    )
+
+    # データ更新周期
+    period = 1 / baudrate
+    # 1ビットあたりのクロック数
+    period_count = int(period / (1 / clk_freq))
+    # start bit検出した後、1/2周期待った地点をサンプリングポイントとする
+    sample_point = math.ceil(period_count / 2)
+
+    async def bench(ctx: SimulatorContext):
+        ctx.set(dut.en, 1)
+        ctx.set(dut.rx, 1)
+        ctx.set(dut.stream.ready, 0)
+        await ctx.tick().repeat(10)
+        assert ctx.get(dut.stream.valid) == 0, "idle state error"
+
+        for data_idx, expect_data in enumerate(send_datas):
+            assert ctx.get(dut.stream.valid) == 0, "stream valid state error"
+            logging.debug(f"send data[{data_idx}]: {expect_data:02x}")
+            # start bit
+            ctx.set(dut.rx, 0)
+            await ctx.tick().repeat(period_count)
+            # データビット送信
+            for i in range(num_data_bit):
+                current_bit = (expect_data >> i) & 1
+                ctx.set(dut.rx, current_bit)
+                logging.debug(
+                    f"[{i:02d}] send progress data:{expect_data:02x} rx:{current_bit}"
+                )
+                # assert ctx.get(dut.busy) == 1, "busy state error"
+                await ctx.tick().repeat(period_count)
+            # パリティビット送信
+            if parity != UartParity.NONE:
+                expect_parity = (
+                    odd_parity(expect_data, num_data_bit)
+                    if parity == UartParity.ODD
+                    else even_parity(expect_data, num_data_bit)
+                )
+                ctx.set(dut.rx, expect_parity)
+                logging.debug(f"parity bit: {expect_parity}")
+                await ctx.tick().repeat(period_count)
+            # stop bit
+            for i in range(num_stop_bit):
+                ctx.set(dut.rx, 1)
+                await ctx.tick().repeat(period_count)
+                logging.debug("stop bit: 1")
+            # validになるのを待って受信
+            ctx.set(dut.stream.ready, 1)
+            while ctx.get(dut.stream.valid) == 0:
+                await ctx.tick()
+            actual_data = ctx.get(dut.stream.payload)
+            logging.debug(
+                f"stream payload {actual_data:02x} is captured (expect {expect_data:02x})"
+            )
+            assert actual_data == expect_data, (
+                f"recv data error: expect {expect_data:02x}, actual {actual_data:02x}"
+            )
+            # Captureされたらreadyを下げ, validもないはず
+            await ctx.tick()
+            assert ctx.get(dut.stream.valid) == 0, "stream valid state error"
+            ctx.set(dut.stream.ready, 0)
+            await ctx.tick()
+
+    Simulation.run(
+        name=f"{run_uart_rx_recv.__name__}_baudrate{baudrate}_num_data_bit{num_data_bit}_num_stop_bit{num_stop_bit}_parity{parity}",
+        dut=dut,
+        testbench=bench,
+        clock=clk_freq,
+    )
+
+
+@pytest.mark.parametrize(
+    "clk_freq, baudrate, num_data_bit, num_stop_bit, parity",
+    [
+        (10e6, 115200, 8, 1, UartParity.NONE),
+        (100e6, 115200, 8, 1, UartParity.NONE),
+        (100e6, 921600, 8, 1, UartParity.NONE),
+    ],
+)
+def test_uart_tx_send_short(
     clk_freq: float,
     baudrate: int,
     num_data_bit: int,
