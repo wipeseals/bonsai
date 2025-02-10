33ac4d5172f48a3fdfa2e93ae1e08c89baf8d1c4
wipeseals <wipeseals@gmail.com>
2025-02-10 22:12:56 +0900

    feat(wip): 合成とおらない

--- bonsai/top.py
+++ bonsai/top.py
@@ -29,35 +29,24 @@ from periph.uart import UartConfig, UartRx, UartTx
 from periph.video import VgaConfig, VgaOut
 
 
-class Top(wiring.Component):
-    def __init__(
-        self,
-        *,
-        src_loc_at=0,
-    ):
-        periph_clk_freq: float = 27e6
-        self.timer = Timer(clk_freq=periph_clk_freq, default_period_seconds=1.0)
-        self.uart_tx = UartTx(config=UartConfig.from_freq(clk_freq=periph_clk_freq))
-        self.uart_rx = UartRx(config=UartConfig.from_freq(clk_freq=periph_clk_freq))
-
-        super().__init__(
-            {
-                "O_psram_cs_n": Out(1),
-                "O_psram_ck": Out(1),
-                "O_psram_ck_n": Out(1),
-                "IO_psram_dq": In(8),
-                "IO_psram_rwds": In(1),
-                "O_psram_reset_n": Out(1),
-            }
-        )
-
+class Top(Elaboratable):
     def elaborate(self, platform: Platform) -> Module:
         # TODO: other platform specific logic
         assert isinstance(platform, TangNano9kPlatform), (
             f"Unsupported platform: {platform}"
         )
+        periph_clk_freq: float = 27e6
 
         m = Module()
+        m.submodules.timer = timer = Timer(
+            clk_freq=periph_clk_freq, default_period_seconds=1.0
+        )
+        m.submodules.uart_tx = uart_tx = UartTx(
+            config=UartConfig.from_freq(clk_freq=periph_clk_freq)
+        )
+        m.submodules.uart_rx = uart_rx = UartRx(
+            config=UartConfig.from_freq(clk_freq=periph_clk_freq)
+        )
 
         ##################################################################
         # Clock Setup
@@ -100,38 +89,35 @@ class Top(wiring.Component):
         ]
         ##################################################################
         # Timer
-        m.submodules.timer = self.timer
         timer_toggle_sig = Signal(1, 0)
-        with m.If(self.timer.ovf):
+        with m.If(timer.ovf):
             m.d.sync += timer_toggle_sig.eq(~timer_toggle_sig)
         m.d.comb += [
             # Internal
-            self.timer.en.eq(1),
-            self.timer.clr.eq(0),
-            self.timer.trig.eq(0),
-            self.timer.cmp_count_wr.eq(0),
-            self.timer.mode.eq(TimerMode.FREERUN_WITH_CLEAR),
+            timer.en.eq(1),
+            timer.clr.eq(0),
+            timer.trig.eq(0),
+            timer.cmp_count_wr.eq(0),
+            timer.mode.eq(TimerMode.FREERUN_WITH_CLEAR),
         ]
 
         ##################################################################
         # uart loopback
-        m.submodules.uart_tx = self.uart_tx
-        m.submodules.uart_rx = self.uart_rx
         uart = platform.request("uart", 0, dir="-")
         m.submodules.uart_tx_pin = uart_tx_pin = io.Buffer("o", uart.tx)
         m.submodules.uart_rx_pin = uart_rx_pin = io.Buffer("i", uart.rx)
         m.d.comb += [
             # External pins
-            uart_tx_pin.o.eq(self.uart_tx.tx),
-            self.uart_rx.rx.eq(uart_rx_pin.i),
+            uart_tx_pin.o.eq(uart_tx.tx),
+            uart_rx.rx.eq(uart_rx_pin.i),
             # Internal RX
-            self.uart_rx.en.eq(1),
+            uart_rx.en.eq(1),
             # Internal TX
-            self.uart_tx.en.eq(1),
+            uart_tx.en.eq(1),
             # RX->TX Loopback
-            self.uart_tx.stream.payload.eq(self.uart_rx.stream.payload),
-            self.uart_tx.stream.valid.eq(self.uart_rx.stream.valid),
-            self.uart_rx.stream.ready.eq(self.uart_tx.stream.ready),
+            uart_tx.stream.payload.eq(uart_rx.stream.payload),
+            uart_tx.stream.valid.eq(uart_rx.stream.valid),
+            uart_rx.stream.ready.eq(uart_tx.stream.ready),
         ]
 
         ##################################################################
@@ -164,21 +150,32 @@ class Top(wiring.Component):
                     }
                 )
 
+        # PSRAM W955D8MBYA6I が 2つある
         # Chip Select#
-        mem_cs_n = Signal(1)
+        mem_cs_n_0 = Signal(1)
+        mem_cs_n_1 = Signal(1)
         # Differential Clock
-        mem_clk = o_clkout
-        mem_clk_n = ~o_clkout
+        mem_clk_0 = o_clkout
+        mem_clk_n_0 = ~o_clkout
+        mem_clk_1 = o_clkout
+        mem_clk_n_1 = ~o_clkout
         # Data Input/Output
-        mem_dq_o = Signal(8)
-        mem_dq_i = Signal(8)
-        mem_dq_oe = Signal(1)
+        mem_dq_o_0 = Signal(8)
+        mem_dq_i_0 = Signal(8)
+        mem_dq_oe_0 = Signal(1)
+        mem_dq_o_1 = Signal(8)
+        mem_dq_i_1 = Signal(8)
+        mem_dq_oe_1 = Signal(1)
         # Read Write Data Strobe
-        mem_rwds_o = Signal(1)
-        mem_rwds_i = Signal(1)
-        mem_rwds_en = Signal(1)
+        mem_rwds_o_0 = Signal(1)
+        mem_rwds_i_0 = Signal(1)
+        mem_rwds_en_0 = Signal(1)
+        mem_rwds_o_1 = Signal(1)
+        mem_rwds_i_1 = Signal(1)
+        mem_rwds_en_1 = Signal(1)
         # Hardware Reset#
-        mem_reset_n = Signal(1)
+        mem_reset_n_0 = Signal(1)
+        mem_reset_n_1 = Signal(1)
 
         # mem_ca_data = Signal(CommandAddressData)
         # mem_cmd_0 = mem_ca_data.bit_select(40, 8)
@@ -190,30 +187,45 @@ class Top(wiring.Component):
 
         # for GN1NR-9C internal PSRAM
         # 規定の名前でTopに出しておくと合成されるらしい
+
         # output [1:0] O_psram_cs_n
-        m.d.O_psram_cs_n = O_psram_cs_n = IOBufferInstance(
-            self.O_psram_cs_n, o=mem_cs_n, oe=Const(1)
+        m.submodules.O_psram_cs_n = O_psram_cs_n = io.Buffer(
+            "o",
+            io.SingleEndedPort(
+                IOPort(2, name="O_psram_cs_n"), direction=io.Direction.Output
+            ),
         )
-        # output [1:0] O_psram_ck
-        m.d.O_psram_ck = O_psram_ck = IOBufferInstance(
-            self.O_psram_ck, o=mem_clk, oe=Const(1)
-        )
-        # output [1:0] O_psram_ck_n
-        m.d.O_psram_ck_n = O_psram_ck_n = IOBufferInstance(
-            self.O_psram_ck_n, o=mem_clk_n, oe=Const(1)
-        )
-        # inout [15:0] IO_psram_dq
-        m.d.IO_psram_dq = IO_psram_dq = IOBufferInstance(
-            self.IO_psram_dq, i=mem_dq_i, o=mem_dq_o, oe=mem_dq_oe
-        )
-        # inout [1:0] IO_psram_rwds
-        m.d.IO_psram_rwds = IO_psram_rwds = IOBufferInstance(
-            self.IO_psram_rwds, i=mem_rwds_i, o=mem_rwds_o, oe=mem_rwds_en
-        )
-        # output [1:0] O_psram_reset_n
-        m.d.O_psram_reset_n = O_psram_reset_n = IOBufferInstance(
-            self.O_psram_reset_n, o=mem_reset_n, oe=Const(1)
-        )
+        # # output [1:0] O_psram_ck
+        # # output [1:0] O_psram_ck_n
+        # m.submodules.O_psram_ck = O_psram_ck = io.Buffer(
+        #     "o",
+        #     io.DifferentialPort(
+        #         p=IOPort(2, name="O_psram_ck"),
+        #         n=IOPort(2, name="O_psram_ck_n"),
+        #         direction=io.Direction.Output,
+        #     ),
+        # )
+        # # inout [15:0] IO_psram_dq
+        # m.submodules.IO_psram_dq = IO_psram_dq = io.Buffer(
+        #     "io",
+        #     io.SingleEndedPort(
+        #         IOPort(16, name="IO_psram_dq"), direction=io.Direction.Bidir
+        #     ),
+        # )
+        # # inout [1:0] IO_psram_rwds
+        # m.submodules.IO_psram_rwds = IO_psram_rwds = io.Buffer(
+        #     "io",
+        #     io.SingleEndedPort(
+        #         IOPort(2, name="IO_psram_rwds"), direction=io.Direction.Bidir
+        #     ),
+        # )
+        # # output [1:0] O_psram_reset_n
+        # m.submodules.O_psram_reset_n = O_psram_reset_n = io.Buffer(
+        #     "o",
+        #     io.SingleEndedPort(
+        #         IOPort(2, name="O_psram_reset_n"), direction=io.Direction.Output
+        #     ),
+        # )
 
         ##################################################################
         # VGA
