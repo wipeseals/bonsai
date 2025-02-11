fac841ef4b61ceffe08df3bc27833990c51aa809
wipeseals <wipeseals@gmail.com>
2025-02-11 20:18:17 +0900

    feat: SPI MasterのStreamIn/Out対応

--- bonsai/periph/spi.py
+++ bonsai/periph/spi.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 from amaranth import Module, Signal
 from amaranth.build.plat import Platform
-from amaranth.lib import wiring, stream
+from amaranth.lib import stream, wiring
 from amaranth.lib.wiring import In, Out
 from amaranth.utils import ceil_log2
 
@@ -43,8 +43,8 @@ class SpiMaster(wiring.Component):
             {
                 # for internal
                 "en": In(1),
-                "din": In(stream.Signature(config.data_width)),
-                "dout": Out(stream.Signature(config.data_width)),
+                "data_mosi": In(stream.Signature(config.data_width)),
+                "data_miso": Out(stream.Signature(config.data_width)),
                 "busy": Out(1),  # status
                 "done": Out(1),  # status
                 # for external
@@ -58,31 +58,34 @@ class SpiMaster(wiring.Component):
     def elaborate(self, platform: Platform) -> Module:
         m = Module()
 
-        # SCLK (SYSCLK/2)
-        sclk_cpol0 = Signal(1, init=0)
-        sclk_cpol0_riging = Signal(1, init=0)
-        sclk_cpol0_falling = Signal(1, init=0)
+        # SCLK
+        sclk_is_rising = Signal(1, init=0)
+        sclk_is_falling = Signal(1, init=0)
 
         sclk_en = Signal(1, init=0)
         with m.If(sclk_en):
             m.d.sync += [
                 # Edge detect
-                sclk_cpol0_riging.eq(sclk_cpol0 == 0),  # (Current)Low -> High
-                sclk_cpol0_falling.eq(sclk_cpol0 == 1),  # (Current)High -> Low
+                sclk_is_rising.eq(self.sclk == 0),  # (Current)Low -> High
+                sclk_is_falling.eq(self.sclk == 1),  # (Current)High -> Low
                 # Next SCLK
-                sclk_cpol0.eq(~sclk_cpol0),
+                self.sclk.eq(~self.sclk),
             ]
         with m.Else():
             m.d.sync += [
                 # Disable Edge
-                sclk_cpol0_riging.eq(0),
-                sclk_cpol0_falling.eq(0),
+                sclk_is_rising.eq(0),
+                sclk_is_falling.eq(0),
                 # Disable SCLK
-                sclk_cpol0.eq(0),  # CPOL0: Normally low
+                self.sclk.eq(0),  # CPOL0: Normally low
             ]
         # MOSI/MISO
         mosi_reg = Signal(self._config.data_width, reset=0)
         miso_reg = Signal(self._config.data_width, reset=0)
+        m.d.comb += [
+            self.mosi.eq(mosi_reg[-1]),
+            # miso_reg は rising edgeで更新
+        ]
 
         # Transfer FSM
         transfer_counter = Signal(self._config.transfer_counter_width, init=0)
@@ -90,23 +93,53 @@ class SpiMaster(wiring.Component):
             with m.State("IDLE"):
                 # Default
                 m.d.sync += [
+                    # transfer regs clear
                     mosi_reg.eq(0),
                     miso_reg.eq(0),
                     transfer_counter.eq(0),
+                    # disable SCLK
                     sclk_en.eq(0),
+                    # flag clear
                     self.busy.eq(0),
                     self.done.eq(0),
                 ]
 
-                # enable & din=valid で開始
-                with m.If(self.en & self.din.valid):
+                # 前回送信したデータの受取確認
+                with m.If(self.data_miso.valid):
+                    with m.If(self.data_miso.ready):
+                        # Data captureされるので無効データ化
+                        m.d.sync += [
+                            # Stream In: Allow
+                            self.data_mosi.ready.eq(1),
+                            # Stream Out: Valid->Invalid
+                            self.data_miso.valid.eq(0),
+                        ]
+                    with m.Else():
+                        # 受け取れていないので待機
+                        m.d.sync += [
+                            # Stream In: Keep Deny
+                            self.data_mosi.ready.eq(0),
+                            # Stream Out: keep Valid
+                            self.data_miso.valid.eq(1),
+                        ]
+                with m.Else():
+                    # 受取済 or 1度も送信していない
                     m.d.sync += [
-                        # Stream In: Capture
-                        self.din.ready.eq(1),
-                        # Stream Out: N/C
-                        self.dout.valid.eq(0),
-                        # Data Reg: Capture din -> mosi_reg
-                        mosi_reg.eq(self.din.payload),
+                        # Stream In: Allow
+                        self.data_mosi.ready.eq(1),
+                        # Stream Out: Invalid
+                        self.data_miso.valid.eq(0),
+                    ]
+
+                # enable かつ mosi のデータ転送タイミング(ready/valid)が合致したら送信開始
+                with m.If(self.en & self.data_mosi.valid & self.data_mosi.ready):
+                    m.d.sync += [
+                        # Stream In: Captured
+                        self.data_mosi.ready.eq(0),
+                        # Stream Out: keep Disable
+                        self.data_miso.valid.eq(0),
+                        # Data Reg: Capture data_mosi -> mosi_reg
+                        mosi_reg.eq(self.data_mosi.payload),  # MSBが次cycそのまま出力
                         miso_reg.eq(0),
                         # Transfer Counter: Initial state + Enable SCLK
                         transfer_counter.eq(0),
@@ -118,12 +151,12 @@ class SpiMaster(wiring.Component):
                     m.next = "XFER"
             with m.State("XFER"):
                 # Rise -> Fall -> Rise -> Fall -> ... -> Fall(last) -> Done
-                with m.If(sclk_cpol0_riging):
+                with m.If(sclk_is_rising):
                     # SCLK Rising Edge: サンプリング
                     m.d.sync += [
                         miso_reg.eq(miso_reg << 1 | self.miso),
                     ]
-                with m.If(sclk_cpol0_falling):
+                with m.If(sclk_is_falling):
                     # SCLK Falling Edge: データシフト
                     m.d.sync += [
                         mosi_reg.eq(mosi_reg << 1),
@@ -144,8 +177,12 @@ class SpiMaster(wiring.Component):
             with m.State("DONE"):
                 # データ出力+フラグ更新
                 m.d.sync += [
-                    # Data Reg: Capture miso_reg -> dout
-                    self.dout.eq(miso_reg),
+                    # Stream In: Keep Deny
+                    self.data_mosi.ready.eq(0),
+                    # Stream Out: Valid
+                    self.data_miso.valid.eq(1),
+                    # Data Reg: Capture miso_reg -> data_miso
+                    self.data_miso.eq(miso_reg),
                     # Flags: Done
                     self.busy.eq(0),
                     self.done.eq(1),
