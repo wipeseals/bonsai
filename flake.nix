fb6ca9b9557c87103964243c8b39fec830833b8e
wipeseals <wipeseals@gmail.com>
2025-02-26 00:51:54 +0900

    build(wip): amaranth build用nix file

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "bonsai project development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }: let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells = {
+      x86_64-linux = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.pypy310
+            pkgs.uv
+            pkgs.yosys
+            pkgs.sby
+            pkgs.verilator
+          ];
+          shellHook = ''
+            echo "Welcome to the bonsai project!"
+          '';
+        };
+      };
+    };
+  };
+}
